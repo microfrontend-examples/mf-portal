31a3a2b4cad5cfbc762ffeb9d4235ee853c56645
Annas Alghoffar <aag.annas@gmail.com>
2025-02-01 02:46:12 +0700

    feat: test update base route

--- src/root.tsx
+++ src/root.tsx
@@ -13,7 +13,7 @@ export const Root = (props: { clerkPubKey: string }) => {
     return (
         <StrictMode>
             <ClerkProvider publishableKey={props.clerkPubKey} afterSignOutUrl="/">
-                <BrowserRouter basename="portal">
+                <BrowserRouter basename="portalz">
                     <Routes>
                         <Route element={<Page />}>
                             <Route index element={<Index />} />
